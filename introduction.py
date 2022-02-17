f2574d1a1025b35bcb14fe42ec26cd3b14d00ef8
SagarSrinivas <68839417+sagarsrinivas3@users.noreply.github.com>
2022-02-17 08:18:07 +0000

    Initial commit

---
+++ introduction.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+myfile = 'files/sample.txt'
+
+#looping a file line by line
+with open(myfile, 'r') as f:
+  for line in f:
+    print(line[:-1])
+
+#looping a file line by line
+with open(myfile, 'r') as f:
+  print(f.read())
+
+# read a file via pathlib
+p1 = Path('files/sample.txt')
+
+#check a file exist or not
+if p1.exists():
+  print("File exists!!")
+else :
+  print("File not found.")
+
+# extract file name 
+print(p1.name)
+
+# extract file stem 
+print(p1.stem)
+
+# extract file suffix 
+print(p1.suffix)
+
+# get list of files in dir
+# iteam is of pathlib.posixpath type
+p2 = Path('files/')
+for iteam in p2.iterdir():
+  print(iteam)
